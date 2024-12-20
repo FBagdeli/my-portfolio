6bed7f1d5a87366b3c3b74813a808732a113c0f7
Farshad <f.bagdeli13@gmail.com>
2024-12-20 11:35:49 +0100

    feat: add responsive styles and functionality to hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,11 +5,14 @@ const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
+      
         <h1 className={styles.title}>Hi, I’m Farshad</h1>
         <p className={styles.description}>I’m a passionate full-stack developer dedicated to crafting efficient and scalable solutions. I thrive on building robust applications and writing clean, maintainable code.</p>
+        <a href="mailto:f.bagdeli13@outlook.com" className={styles.contactBtn}>Contact Me</a>
       </div>
-      <a href="mailto:f.bagdeli13@outlook.com" className={styles.contactBtn}>Contact Me</a>
+      
       <img src={getImageUrl('avatar.png')} alt="my image" className={styles.avatarImg}/>
+      
       <div className={styles.topBlur}/>
       <div className={styles.bottomBlur}/>
     </section>
