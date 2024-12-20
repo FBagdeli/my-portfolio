24c992b29d4e5553cf839eaafc2fad3e2de1771b
Farshad <f.bagdeli13@gmail.com>
2024-12-20 09:47:00 +0100

    feat: add responsive styles and menu button functionality for navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from "./Navbar.module.css"
+import { getImageUrl } from "../../utils.js"
 
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <a href="/" className={styles.title}>Portfolio</a>
       <div className={styles.menu}>
+        <img className={styles.menuBtn} src={getImageUrl("menuIcon.png")} alt="menu icon" />
         <ul className={styles.menuItems}>
           <li><a href="">About</a></li>
           <li><a href="">Experience</a></li>
