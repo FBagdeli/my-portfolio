8ba4efbe8dbc65f084cfb4f1cf29e70f7d58e5ce
Farshad <f.bagdeli13@gmail.com>
2024-12-20 10:27:35 +0100

    feat: add menu functionality, add closeIcon to assets

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,16 +3,17 @@ import styles from "./Navbar.module.css"
 import { getImageUrl } from "../../utils.js"
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false)
   return (
     <nav className={styles.navbar}>
       <a href="/" className={styles.title}>Portfolio</a>
       <div className={styles.menu}>
-        <img className={styles.menuBtn} src={getImageUrl("menuIcon.png")} alt="menu icon" />
-        <ul className={styles.menuItems}>
-          <li><a href="">About</a></li>
-          <li><a href="">Experience</a></li>
-          <li><a href="">Projects</a></li>
-          <li><a href="">Contact</a></li>
+        <img className={styles.menuBtn} src={menuOpen ?getImageUrl("closeIcon.png") : getImageUrl("menuIcon.png") } alt="menu icon" onClick={() => setMenuOpen(!menuOpen)} />
+        <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`} onClick={() => setMenuOpen(false)}>
+          <li><a href="about">About</a></li>
+          <li><a href="experience">Experience</a></li>
+          <li><a href="projects">Projects</a></li>
+          <li><a href="contact">Contact</a></li>
         </ul>
       </div>
     </nav>
